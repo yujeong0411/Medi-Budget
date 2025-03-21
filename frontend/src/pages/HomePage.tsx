15ae00eceff11c8e77b3445fd9619268051bf5f2
최유정 <choiyujeong0411@gmail.com>
2025-03-21 17:48:03 +0900

    design: 영상 텍스트 추가

--- frontend/src/pages/HomePage.tsx
+++ frontend/src/pages/HomePage.tsx
@@ -1,4 +1,5 @@
 import Logo from '@/components/logo.tsx';
+import logo from '@/assets/logo.png'
 import { useRef, useEffect } from 'react';
 
 function HomePage() {
@@ -29,14 +30,32 @@ function HomePage() {
           playsInline
           className="absolute top-0 left-0 w-full h-full object-cover z-[-1]"
         >
-          <source src={videoFile} type="video/mp4" />
+          {/*<source src={videoFile} type="video/mp4" />*/}
           "Your browser does not support the video tag."
         </video>
+
+        {/* Dark Overlay */}
+        <div className="absolute top-0 left-0 w-full h-full bg-black opacity-60 z-1"></div>
         {/*비디오 위에 표시할 콘텐츠*/}
-        <div>
-          <h1>텍스트</h1>
+        <div className="relative z-2 flex flex-col items-center justify-center h-full text-white p-4">
+          <div className="max-w-4xl mx-auto text-center">
+            <h2 className="text-3xl font-bold mb-8">병원 방문 전, 이런 고민 해보셨나요?</h2>
+            <div className="text-2xl font-bold mb-8">
+              <p>"이 증상으로 병원에 가면 진료비가 얼마나 나올까?"</p>
+              <p>"독감진료는 얼마를 내지?"</p>
+              <p>"아이는 감기 진료가 얼마나 나올지 모르겠어..."</p>
+            </div>
+            <p className="text-2xl mt-8">
+              Medi Budget은 진료비의 불확실성을 해소하고, 의료비에 대한 투명성을 높여 합리적인 의료 소비를 돕습니다.
+            </p>
+          </div>
         </div>
       </section>
+
+      {/*검색 창*/}
+     <section className="w-full bg-indigo-">
+
+     </section>
     </div>
   );
 }
